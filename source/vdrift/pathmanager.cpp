97e90f3aba1bf56933db9b8654156cd10fe5bf67
Tapio Vierros <tapiovierros@gmail.com>
2011-03-22 17:45:30 +0200

    Massive overhaul of PATHMANAGER class.

--- source/vdrift/pathmanager.cpp
+++ source/vdrift/pathmanager.cpp
@@ -1,99 +1,158 @@
+/// Big portions of this file are borrowed and adapted from Performous under GPL (http://performous.org)
+
 #include "stdafx.h"
 
 #include "pathmanager.h"
-//#include "definitions.h"
+#include <boost/filesystem.hpp>
 
-#define SETTINGS_DIR ".vdrift"
+#ifdef _WIN32
+#include <windows.h>
+#include <shlobj.h>
+#endif
 
-//#define DATA_DIR "/usr/local/share/games/vdrift/data"
-#define DATA_DIR "data"
+#ifndef SHARED_DATA_DIR
+#define SHARED_DATA_DIR "data"
+#endif
 
+// TODO: Create a PORTABLE_INSTALL flag that allows disabling the usage of system dirs
+
+// Define this useful alias for the overlong namespace name
+namespace fs = boost::filesystem;
+
+namespace {
+	fs::path execname();
+}
+
+/*static*/ std::string PATHMANAGER::home_dir;
+/*static*/ std::string PATHMANAGER::user_config_dir;
+/*static*/ std::string PATHMANAGER::game_config_dir;
+/*static*/ std::string PATHMANAGER::user_data_dir;
+/*static*/ std::string PATHMANAGER::game_data_dir;
+/*static*/ std::string PATHMANAGER::profile_suffix;
 
 void PATHMANAGER::Init(std::ostream & info_output, std::ostream & error_output)
 {
-	//figure out the user's home directory
-	char *homedir;
-	#ifndef _WIN32
-	homedir = getenv ( "HOME" );
-	if ( homedir == NULL )
+	// Figure out the user's home directory
 	{
-		homedir = getenv ( "USER" );
-		if ( homedir == NULL )
-		{
-			homedir = getenv ( "USERNAME" );
-			if ( homedir == NULL )
+		home_dir = "";
+		#ifndef _WIN32 // POSIX
+			char *homedir = getenv("HOME");
+			if (homedir == NULL)
 			{
-				error_output << "Could not find user's home directory!" << std::endl;
+				home_dir = "/home/";
+				homedir = getenv("USER");
+				if (homedir == NULL) {
+					homedir = getenv("USERNAME");
+					if (homedir == NULL) {
+						error_output << "Could not find user's home directory!" << std::endl;
+						home_dir = "/tmp/";
+					}
+				}
 			}
-		}
-		home_directory = "/home/";
-	}
-	#else
-	homedir = getenv ( "USERPROFILE" );
-	if ( homedir == NULL )
-	{
-		homedir = "data"; // WIN 9x/Me
-	}
-	#endif
-	home_directory += homedir;
-	
-	//find data dir
-	char *datadir = getenv ( "VDRIFT_DATA_DIRECTORY" );
-	if (datadir == NULL) {
-		#ifndef _WIN32
-		if (FileExists("data/settings/options.config")) {
-			data_directory = "data";
-		} else {
-			data_directory = DATA_DIR;
-		}
-		#else
-		data_directory = "data";
+		#else // Windows
+			char *homedir = getenv("USERPROFILE");
+			if (homedir == NULL) homedir = "data"; // WIN 9x/Me
 		#endif
-	} else {
-		data_directory = (std::string) datadir;
+		home_dir += homedir;
 	}
-	
-	//find settings file
-	settings_path = home_directory;
-	#ifndef _WIN32
-	settings_path += "/";
-	settings_path += SETTINGS_DIR;
-	MakeDir(settings_path);
-	#else
+
+	// Find user's config dir
+	#ifndef _WIN32 // POSIX
+	{
+		char const* conf = getenv("XDG_CONFIG_HOME");
+		if (conf) user_config_dir = (fs::path(conf) / "stuntrally").string();
+		else user_config_dir = (fs::path(home_dir) / ".config" / "stuntrally").string();
+	}
+	#else // Windows
+	{
+		// Open AppData directory
+		std::string str;
+		ITEMIDLIST* pidl;
+		char AppDir[MAX_PATH];
+		HRESULT hRes = SHGetSpecialFolderLocation(NULL, CSIDL_APPDATA|CSIDL_FLAG_CREATE , &pidl);
+		if (hRes == NOERROR)
+		{
+			SHGetPathFromIDList(pidl, AppDir);
+			int i;
+			for (i = 0; AppDir[i] != '\0'; i++) {
+				if (AppDir[i] == '\\') str += '/';
+				else str += AppDir[i];
+			}
+			user_config_dir = (fs::path(str) / "stuntrally").string();
+		}
+	}
+	#endif
+
+	// Create user's config dir
+	try {
+		fs::create_directory(user_config_dir);
+	} catch (...) {
+		std::cerr << "Could not create configuration directory " << user_config_dir << std::endl;
+	}
+
+	// Find user's data dir (for additional data)
+	// Not used currently
 	#ifdef _WIN32
-	MakeDir(settings_path+"\\My Documents");
-	MakeDir(settings_path+"\\My Documents\\My Games");
-	settings_path += "\\My Documents\\My Games\\VDrift";
-	MakeDir(settings_path);
+	user_data_dir = user_config_dir;  // APPDATA/stuntrally
 	#else
 	{
-		settings_path += "/";
-		settings_path += SETTINGS_DIR;
+		fs::path shortDir = "stuntrally";
+		fs::path shareDir = SHARED_DATA_DIR;
+		char const* xdg_data_home = getenv("XDG_DATA_HOME");
+		user_data_dir = (xdg_data_home ? xdg_data_home / shortDir : fs::path(home_dir) / ".local" / shareDir).string();
 	}
 	#endif
-	#endif
-	
-	//MakeDir(GetTrackRecordsPath());
-	//MakeDir(GetReplayPath());
 
-	//print diagnostic info
-	info_output << "Home directory: " << home_directory << std::endl;
-	bool settings_file_present = FileExists(GetSettingsFile());
-	info_output << "Settings file: " << GetSettingsFile();
-	if (!settings_file_present)
-		info_output << " (does not exist, will be created)";
-	info_output << std::endl;
-	info_output << "Data directory: " << data_directory;
-	if (datadir)
-		info_output << "\nVDRIFT_DATA_DIRECTORY: " << datadir;
-#ifndef _WIN32
-	info_output << "\nDATA_DIR: " << DATA_DIR;
-#endif
-	info_output << std::endl;
+	// Find game data dir and defaults config dir
+	char *datadir = getenv("STUNTRALLY_DATA_ROOT");
+	if (datadir) {
+		game_data_dir = std::string(datadir);
+	} else {
+		fs::path shortDir = "stuntrally";
+		fs::path shareDir = SHARED_DATA_DIR;
+		typedef std::vector<fs::path> Paths;
+		Paths dirs;
+
+		// Adding users data dir
+		// TODO: Disabled for now until this is handled properly
+		//dirs.push_back(user_data_dir);
+
+		// Adding relative path from installed executable
+		dirs.push_back(execname().parent_path().parent_path() / shareDir);
+		// Adding relative path for running from sources
+		dirs.push_back(execname().parent_path().parent_path() / "data");
+		dirs.push_back(execname().parent_path().parent_path());
+		#ifndef _WIN32
+		// Adding XDG_DATA_DIRS
+		{
+			char const* xdg_data_dirs = getenv("XDG_DATA_DIRS");
+			std::istringstream iss(xdg_data_dirs ? xdg_data_dirs : "/usr/local/share/:/usr/share/");
+			for (std::string p; std::getline(iss, p, ':'); dirs.push_back(p / shortDir)) {}
+		}
+		#endif
+		// TODO: Adding path from config file
+
+		// Loop through the paths and pick the first one that contain some data
+		for (Paths::const_iterator p = dirs.begin(); p != dirs.end(); ++p) {
+			// Data dir
+			if (fs::exists(*p / "hud")) game_data_dir = p->string();
+			// Config dir
+			if (fs::exists(*p / "config")) game_config_dir = (*p / "config").string();
+			// Check if both are found
+			if (!game_data_dir.empty() && !game_config_dir.empty()) break;
+		}
+	}
+
+	// Print diagnostic info
+	info_output << "Home directory: " << home_dir << std::endl;
+	info_output << "User config directory: " << GetUserConfigDir() << std::endl;
+	info_output << "Config defaults directory: " << GetGameConfigDir() << std::endl;
+	info_output << "Data directory: " << GetDataPath() << std::endl;
 	info_output << "Log file: " << GetLogFile() << std::endl;
 }
 
-bool PATHMANAGER::GetFolderIndex(std::string folderpath, std::list <std::string> & outputfolderlist, std::string extension) const
+// TODO: This is probably far easier and more elegant to implement with boost::filesystem
+bool PATHMANAGER::GetFolderIndex(std::string folderpath, std::list <std::string> & outputfolderlist, std::string extension)
 {
 //------Folder listing code for POSIX
 #ifndef _WIN32
@@ -165,4 +224,62 @@ bool PATHMANAGER::GetFolderIndex(std::string folderpath, std::list <std::string>
 	
 	outputfolderlist.sort();
 	return true;
+}
+
+namespace {
+	/// Get the current executable name with path. Returns empty path if the name
+	/// cannot be found. May return absolute or relative paths.
+	#if defined(_WIN32)
+	#include <windows.h>
+	fs::path execname() {
+		char buf[1024];
+		DWORD ret = GetModuleFileName(NULL, buf, sizeof(buf));
+		if (ret == 0 || ret == sizeof(buf)) return fs::path();
+		return buf;
+	}
+	#elif defined(__APPLE__)
+	#include <mach-o/dyld.h>
+	fs::path execname() {
+		char buf[1024];
+		uint32_t size = sizeof(buf);
+		int ret = _NSGetExecutablePath(buf, &size);
+		if (ret != 0) return fs::path();
+		return buf;
+	}
+	#elif defined(sun) || defined(__sun)
+	#include <stdlib.h>
+	fs::path execname() {
+		return getexecname();
+	}
+	#elif defined(__FreeBSD__)
+	#include <sys/sysctl.h>
+	fs::path execname() {
+		int mib[4];
+		mib[0] = CTL_KERN;
+		mib[1] = KERN_PROC;
+		mib[2] = KERN_PROC_PATHNAME;
+		mib[3] = -1;
+		char buf[1024];
+		size_t maxchars = sizeof(buf) - 1;
+		size_t size = maxchars;
+		sysctl(mib, 4, buf, &size, NULL, 0);
+		if (size == 0 || size >= maxchars) return fs::path();
+		buf[size] = '\0';
+		return buf;
+	}
+	#elif defined(__linux__)
+	#include <unistd.h>
+	fs::path execname() {
+		char buf[1024];
+		ssize_t maxchars = sizeof(buf) - 1;
+		ssize_t size = readlink("/proc/self/exe", buf, sizeof(buf));
+		if (size <= 0 || size >= maxchars) return fs::path();
+		buf[size] = '\0';
+		return buf;
+	}
+	#else
+	fs::path execname() {
+		return fs::path();
+	}
+	#endif
 }
--- source/vdrift/pathmanager.h
+++ source/vdrift/pathmanager.h
@@ -7,71 +7,48 @@
 
 class PATHMANAGER
 {
-	private:
-		std::string home_directory;
-		std::string settings_path;
-		std::string data_directory;
-		std::string profile_suffix;
-		
-		#ifndef _WIN32
-		bool DirectoryExists(std::string filename) const
-		{
-			DIR *dp;
-			dp = opendir(filename.c_str());
-			if (dp != NULL) {
-				closedir(dp);
-				return true;
-			} else {
-				return false;
-			}
-		}
-		#endif
-		
-		void MakeDir(const std::string & dir)
-		{
-			#ifndef _WIN32
-			mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-			#else
-			//mkdir(dir.c_str());
-			#endif
-		}
-	
-	public:
-		void Init(std::ostream & info_output, std::ostream & error_output);
-		bool GetFolderIndex(std::string folderpath, std::list <std::string> & outputfolderlist, std::string extension="") const; ///<optionally filter for the given extension
+private:
+	static std::string home_dir;
+	static std::string user_config_dir;
+	static std::string game_config_dir;
+	static std::string user_data_dir;
+	static std::string game_data_dir;
+	static std::string profile_suffix;
 
-		std::string GetDataPath() const {				return data_directory;  }
-		//std::string GetTrackRecordsPath() const {		return settings_path  + "/records" + profile_suffix;  }
-		std::string GetSettingsFile() const {			return "game.cfg";  }  //settings_path + "/VDrift.config"+profile_suffix;  }
-		std::string GetLogFile() const {				return "log.txt";  }  //settings_path + "/log.txt";  }
-		std::string GetTrackPath() const {				return data_directory + "/tracks";  }
-		std::string GetCarPath() const {				return data_directory + "/cars";  }
-		std::string GetCarControlsFile() const {		return "config/controls.cfg";  }  //settings_path+"/controls.config"+profile_suffix;
-		std::string GetDefaultCarControlsFile() const {	return "config/controls.cfg";  }//-
-		std::string GetGenericSoundPath() const {		return data_directory + "/sounds";  }
-		std::string GetDriverPath() const {				return data_directory + "/drivers";  }
-		std::string GetReplayPath() const {				return data_directory + "/replays";  }
-		
-		bool FileExists(const std::string & filename) const
-		{
-			std::ifstream test;
-			test.open(filename.c_str());
-			if (test)
-			{
-				test.close();
-				return true;
-			}
-			else
-				return false;
-		}
+public:
+	static void Init(std::ostream & info_output, std::ostream & error_output);
+	static bool GetFolderIndex(std::string folderpath, std::list <std::string> & outputfolderlist, std::string extension=""); ///<optionally filter for the given extension
 
-		///only call this before Init()
-		void SetProfile ( const std::string& value )
-		{
-			assert(data_directory.empty()); //assert that Init() hasn't been called yet
-			profile_suffix = "."+value;
-		}
-	
+	static std::string GetDataPath() {               return game_data_dir; }
+	static std::string GetUserConfigDir() {          return user_config_dir;  }
+	static std::string GetGameConfigDir() {          return game_config_dir;  }
+	//static std::string GetTrackRecordsPath() const { return user_config_dir  + "/records" + profile_suffix;  }
+	static std::string GetSettingsFile() {           return user_config_dir + "/game.cfg"; }
+	static std::string GetLogFile() {                return user_config_dir + "/log.txt"; }
+	static std::string GetTrackPath() {              return game_data_dir + "/tracks"; }
+	static std::string GetCarPath() {                return game_data_dir + "/cars"; }
+	static std::string GetCarControlsFile() {        return user_config_dir + "/controls.cfg"; }
+	static std::string GetDefaultCarControlsFile() { return game_config_dir + "/controls.cfg"; }
+	static std::string GetGenericSoundPath() {       return game_data_dir + "/sounds"; }
+	static std::string GetDriverPath() {             return game_data_dir + "/drivers"; }
+	static std::string GetReplayPath() {             return game_data_dir + "/replays"; }
+
+	static bool FileExists(const std::string & filename)
+	{
+		std::ifstream test(filename.c_str());
+		if (test) return true;
+		else return false;
+	}
+
+
+	// FIXME: Do we intend to use profiles?
+	/// Only call this before Init()
+	static void SetProfile(const std::string& value)
+	{
+		assert(game_data_dir.empty()); // Assert that Init() hasn't been called yet
+		profile_suffix = "." + value;
+	}
+
 };
 
 #endif
