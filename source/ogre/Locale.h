3a302cea7d448069c5dff1e5a9d01de51081042c
Tapio Vierros <tapiovierros@gmail.com>
2011-04-12 18:03:32 +0300

    Disable language detection, since the translation system is in crappy state.

--- source/ogre/Locale.h
+++ source/ogre/Locale.h
@@ -5,7 +5,10 @@ std::string getSystemLanguage() {
 	const std::string default_lang = "en";
 
 	char *loc = getenv("LC_ALL");
-	if (!loc) loc = getenv("LANG");
+	// FIXME: LANG disabled due to problems with MyGUI translation infra.
+	// You can still test translations byt setting LC_ALL env variable.
+	// E.g: $ LC_ALL=fi_FI ./stuntrally
+	//if (!loc) loc = getenv("LANG");
 	if (!loc) return default_lang;
 	// TODO: Windows?
 
