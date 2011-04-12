6b636652f880b4c2d156fb2662c7fd20c17566f5
Tapio Vierros <tapiovierros@gmail.com>
2011-04-12 18:00:09 +0300

    Don't hard-code language choices in C++.

--- source/ogre/Locale.h
+++ source/ogre/Locale.h
@@ -2,17 +2,16 @@
 #include <string>
 
 std::string getSystemLanguage() {
-	const std::string default_lang = "English";
-	
+	const std::string default_lang = "en";
+
 	char *loc = getenv("LC_ALL");
 	if (!loc) loc = getenv("LANG");
 	if (!loc) return default_lang;
 	// TODO: Windows?
 
 	std::string locstr(loc);
-	// Parse locale
-	if (locstr.find("fi_") != std::string::npos) return "Finnish";
-	else if (locstr.find("de_") != std::string::npos) return "German";
-
-	return default_lang;
+	// We parse here only the first part of two part codes (e.g.fi_FI).
+	// We can revisit this if we get regional translations.
+	locstr = locstr.substr(0, 2);
+	return locstr;
 }
