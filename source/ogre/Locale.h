86726fb1bc6bfb156662ec324926f195b213a753
Tapio Vierros <tapiovierros@gmail.com>
2011-04-12 20:46:05 +0300

    Re-enable language auto-detect.

--- source/ogre/Locale.h
+++ source/ogre/Locale.h
@@ -5,10 +5,7 @@ std::string getSystemLanguage() {
 	const std::string default_lang = "en";
 
 	char *loc = getenv("LC_ALL");
-	// FIXME: LANG disabled due to problems with MyGUI translation infra.
-	// You can still test translations byt setting LC_ALL env variable.
-	// E.g: $ LC_ALL=fi_FI ./stuntrally
-	//if (!loc) loc = getenv("LANG");
+	if (!loc) loc = getenv("LANG");
 	if (!loc) return default_lang;
 	// TODO: Windows?
 
