07d8d4b4aad4401dcb58e704bffb42902abc5b5c
scrawl <scrawl@baseoftrash.de>
2011-04-15 22:32:25 +0200

    Fix typo in CarModel.cpp

--- source/ogre/CarModel.cpp
+++ source/ogre/CarModel.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "CarModel.h"
+
+CarModel::CarModel(unsigned int index, const std::string name, Ogre::SceneManager* sceneMgr, SETTINGS* set) : 
+	hue(0), sat(0), val(0), fCam(0), pMainNode(0), pCar(0)
+{
+	iIndex = index;
+	sDirname = name;
+	pSceneMgr = sceneMgr;
+	pSet = set;
+	
+	pReflect = new CarReflection(pSet, iIndex);
+}
+CarModel::~CarModel(void)
+{
+	delete pReflect;
+	///TODO
+}
+void CarModel::Update(void)
+{
+	pReflect->Update();
+}
