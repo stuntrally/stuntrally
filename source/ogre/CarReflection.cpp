35171e7cfb93aa830bffb99868a2e03f10fef8aa
scrawl <scrawl@baseoftrash.de>
2011-04-15 22:34:27 +0200

    Fix CarReflection link error.

--- source/ogre/CarReflection.cpp
+++ source/ogre/CarReflection.cpp
@@ -1,3 +1,16 @@
 
 #include "stdafx.h"
 #include "CarReflection.h"
+
+CarReflection::CarReflection(SETTINGS* set, unsigned int index)
+{
+	///TODO
+}
+CarReflection::~CarReflection()
+{
+	///TODO
+}
+void CarReflection::Update()
+{
+	///TODO
+}
