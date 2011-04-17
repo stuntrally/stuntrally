1781f17965a9f8fe26e216e6887a485565c9640f
Tapio Vierros <tapiovierros@gmail.com>
2011-04-17 17:32:57 +0300

    Add forgotten header.

---
+++ source/network/network.hpp
@@ -0,0 +1,18 @@
+#include "enet-wrapper.hpp"
+#include "protocol.hpp"
+
+class P2PGameClient: public net::NetworkListener {
+public:
+	P2PGameClient(int port = protocol::DEFAULT_PORT);
+
+	void connect(std::string address, int port = protocol::DEFAULT_PORT);
+
+	void connectionEvent(net::NetworkTraffic const& e);
+
+	void disconnectEvent(net::NetworkTraffic const& e);
+
+	void receiveEvent(net::NetworkTraffic const& e);
+
+private:
+	net::NetworkObject m_client;
+};
