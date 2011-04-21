1a2412f4aac26a6649626c6eb427ba216e5d4d25
Tapio Vierros <tapiovierros@gmail.com>
2011-04-21 20:08:20 +0300

    Protocol additions.

--- source/network/protocol.hpp
+++ source/network/protocol.hpp
@@ -2,6 +2,10 @@
 /**
  * This file describes the protocol that is used for network communication.
  * It contains structures and serializations that are transmitted.
+ *
+ * Implementation notes:
+ *   - First member must be uint8_t packet_type
+ *   - Serialization is basically a naïve memcpy, so use only primitive types
  */
 
 #include <stdint.h>
@@ -13,7 +17,7 @@ const unsigned DEFAULT_PORT = 4243;
 
 /**
  * This enum contains all possible message types.
- * It will be transmitted as char.
+ * It will be transmitted as 8-bit unsigned int.
  */
 enum PacketType {
 	HANDSHAKE = 0,
@@ -28,14 +32,6 @@ enum PacketType {
 	GAME_STATUS         // An available game (either client updates, or server reports)
 };
 
-struct Packet {
-	PacketType type;
-	uint32_t length;
-	uint8_t* data;
-
-	Packet(PacketType t, uint32_t l, uint8_t* d): type(t), length(l), data(d) {}
-};
-
 
 /**
  * Contains information about one game that is available for joining.
@@ -45,12 +41,41 @@ struct GameInfo: public net::SimpleSerializer<GameInfo> {
 	uint32_t id;        // Set by server
 	uint32_t address;   // Set by server
 	uint16_t port;      // Set by server
+	uint32_t timestamp; // Set by server
 	uint8_t players;    // Set by client
 	char name[32];      // Set by client
 	char track[32];     // Set by client
+
+	bool operator==(const GameInfo& other) { return id == other.id; }
+	bool operator!=(const GameInfo& other) { return !(*this == other); }
 };
 
 typedef std::map<uint32_t, protocol::GameInfo> GameList;
+
+
+/**
+ * Contains peer/player information.
+ * These structs are passed around to create the complete network topography.
+ */
+struct PeerInfo: public net::SimpleSerializer<PeerInfo> {
+	uint8_t packet_type;
+	uint32_t address;
+	uint16_t port;
+	char name[16];
+
+	bool operator==(const PeerInfo& other) { return address == other.address && port == other.port; }
+	bool operator!=(const PeerInfo& other) { return !(*this == other); }
+};
+
+
+/**
+ * Text message meant to be displayed for user, e.g. chat.
+ */
+struct TextMessage: public net::SimpleSerializer<TextMessage> {
+	uint8_t packet_type;
+	uint32_t length;
+	uint8_t message[256]; //FIXME
+};
 
 
 /**
