a6d7381e7af8ae2164209eccff5db61b06412c75
Tapio Vierros <tapiovierros@gmail.com>
2011-08-29 22:31:58 +0300

    Windows annoyingly is case insensitive.

--- source/vdrift/Buoyancy.cpp
+++ source/vdrift/Buoyancy.cpp
@@ -18,7 +18,7 @@ misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "buoyancy.h"
+#include "Buoyancy.h"
 #include <assert.h>
 
 // Returns the volume of a tetrahedron and updates the centroid accumulator.
