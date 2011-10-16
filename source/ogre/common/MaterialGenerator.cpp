88d08c14ab7b40598e01c14117bd216a3245dd65
scrawl <scrawl@baseoftrash.de>
2011-10-16 15:32:49 +0200

    prepareMaterial()

--- source/ogre/common/MaterialGenerator.cpp
+++ source/ogre/common/MaterialGenerator.cpp
@@ -2,6 +2,7 @@
 #include "MaterialGenerator.h"
 #include "MaterialFactory.h"
 
+#include <OgreMaterialManager.h>
 using namespace Ogre;
 
 MaterialGenerator* MaterialGenerator::setParent(MaterialFactory* parent)
@@ -12,4 +13,15 @@ MaterialGenerator* MaterialGenerator::setParent(MaterialFactory* parent)
 
 MaterialPtr MaterialGenerator::prepareMaterial(const std::string& name)
 {
+	MaterialPtr mat;
+	if (MaterialManager::getSingleton().resourceExists(name))
+	{
+		mat = MaterialManager::getSingleton().getByName(name);
+		mat->removeAllTechniques();
+	}
+	else
+	{
+		mat = MaterialManager::getSingleton().create(name, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+	}
+	return mat;
 }
