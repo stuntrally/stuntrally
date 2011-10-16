18d894c1af2e5ab73dbfe159d698b7bfb341cceb
scrawl <scrawl@baseoftrash.de>
2011-10-16 15:08:20 +0200

    forgot to git add

---
+++ source/ogre/common/CarMaterialGen.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "CarMaterialGen.h"
+
+#include "../Defines.h"
+
+void CarMaterialGenerator::generate()
+{
+	LogO("[CarMaterialGen] generating...");
+}
---
+++ source/ogre/common/CarMaterialGen.h
@@ -0,0 +1,12 @@
+#ifndef CAR_MATERIALGEN_H
+#define CAR_MATERIALGEN_H
+
+#include "MaterialGenerator.h"
+
+class CarMaterialGenerator : public MaterialGenerator
+{
+public:
+	virtual void generate();
+};
+
+#endif
