c80cbbdd8811623324c87e693259299cada19fdf
scrawl <scrawl@baseoftrash.de>
2011-10-22 18:06:07 +0200

    depth shadows on with preset Ultra, changeShadows() in event comboGraphicsAll

--- source/ogre/common/Gui_Common.cpp
+++ source/ogre/common/Gui_Common.cpp
@@ -601,7 +601,7 @@ void App::comboGraphicsAll(ComboBoxPtr cmb, size_t val)
 	case 4:  // Ultra  -------------
 		s.anisotropy = 16;  s.view_distance = 20000;  s.terdetail = 1.0f;  s.terdist = 1000.f;  s.road_dist = 1.2;
 		s.tex_size = 1;  s.ter_mtr = 2;  s.shaders = 1;
-		s.shadow_type = 2;/*3*/  s.shadow_size = 3;  s.shadow_count = 3;  s.shadow_dist = 3000;
+		s.shadow_type = 3;/*3*/  s.shadow_size = 3;  s.shadow_count = 3;  s.shadow_dist = 3000;
 		s.trees = 2.f;  s.grass = 2.f;  s.trees_dist = 2.f;  s.grass_dist = 2.f;	break;
 	}
 #ifndef ROAD_EDITOR  /// game only
@@ -661,6 +661,7 @@ void App::comboGraphicsAll(ComboBoxPtr cmb, size_t val)
 
 	//  update gui  sld,val,chk  ...
 	GuiInitGraphics();  // = newDelegate..?
+	changeShadows(); // apply shadow
 
 	ButtonPtr btn, bchk;  HScrollPtr sl;  size_t v;
 #ifndef ROAD_EDITOR  /// game only
