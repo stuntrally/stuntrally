6a2e03bde1b06012a5156c31bcf11ba461e18770
Tapio Vierros <tapiovierros@gmail.com>
2011-11-28 22:16:32 +0200

    Test using 50-50 of received state and physics engine position estimation.

--- source/vdrift/car.cpp
+++ source/vdrift/car.cpp
@@ -827,7 +827,7 @@ void CAR::UpdateCarState(const protocol::CarStatePackage& state)
 	// for a while, so we use pos/rot only for lazy corrections
 	MATHVECTOR<float,3> curpos = ToMathVector<float>(dynamics.chassis->getCenterOfMassPosition());
 	MATHVECTOR<float,3> errorvec = state.pos - curpos;
-	MATHVECTOR<float,3> newpos = curpos + (errorvec * 0.1f);
+	MATHVECTOR<float,3> newpos = curpos + (errorvec * 0.5f);
 	QUATERNION<float> currot = ToMathQuaternion<float>(dynamics.chassis->getCenterOfMassTransform().getRotation());
 	QUATERNION<float> newrot = currot.QuatSlerp(state.rot, 0.5f);
 
