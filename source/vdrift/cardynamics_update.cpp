1aee3b1cbb872f3087ca98e1673477d3f3b3cad1
scrawl <scrawl@baseoftrash.de>
2011-12-21 12:46:33 +0100

    removed log spam (boost fuel output every frame)

--- source/vdrift/cardynamics_update.cpp
+++ source/vdrift/cardynamics_update.cpp
@@ -307,7 +307,7 @@ void CARDYNAMICS::UpdateBody(T dt, T drive_torque[])
 		if (boostFuel > 3.f)  boostFuel = 3.f;  // par max val
 		// todo: val on hud..
 	}
-	LogO(toStr(boostFuel));
+	//LogO(toStr(boostFuel));
 	///***  -------------------------------------------------------------
 	
 
