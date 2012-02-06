ec5865424b56dca140d04afd5577b2def394a214
scrawl <scrawl@baseoftrash.de>
2012-02-06 14:01:21 +0100

    - shadow filter size default setting & graphics presets - fixed terrain on medium setting

--- source/ogre/common/MaterialGen/TerrainMaterialGen.cpp
+++ source/ogre/common/MaterialGen/TerrainMaterialGen.cpp
@@ -797,7 +797,7 @@ namespace Ogre
 		if(prof->isLayerNormalMappingEnabled() || prof->isLayerParallaxMappingEnabled())
 			ret->setParameter("profiles", "ps_4_0 ps_2_x fp40 arbfp1");  /// removed ps_3_0  to work with depth shadow on dx
 		else
-			ret->setParameter("profiles", "ps_4_0 ps_2_0 fp30 arbfp1");  /// removed ps_3_0  
+			ret->setParameter("profiles", "ps_4_0 ps_2_0 fp40 fp30 arbfp1");  /// removed ps_3_0  
 		ret->setParameter("entry_point", "main_fp");
 
 		return ret;
