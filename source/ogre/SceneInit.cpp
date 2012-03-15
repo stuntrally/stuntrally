ecce598ceb28f67f92a41946b4634dca355d4c26
Crystal Hammer <cryham@gmail.com>
2012-03-15 08:25:56 +0100

    no countdown for replay start

--- source/ogre/SceneInit.cpp
+++ source/ogre/SceneInit.cpp
@@ -250,7 +250,8 @@ void App::LoadGame()  // 2
 	}
 	
 	float pretime = mClient ? 2.0f : pSet->game.pre_time;  // same for all multi players
-	if (mClient) pGame->timer.waiting = true;
+	if (bRplPlay)  pretime = 0.f;
+	if (mClient)  pGame->timer.waiting = true;
 	pGame->NewGameDoLoadMisc(pretime);
 	bool ter = IsTerTrack();
 	sc.ter = ter;
