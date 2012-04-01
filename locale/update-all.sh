ed1516861c5241a8abfbfeb9bfd7e29ec1605ec0
Tapio Vierros <tapiovierros@gmail.com>
2012-04-01 16:18:22 +0300

    Allow skipping translation fetch with --no-fetch parameter.

--- locale/update-all.sh
+++ locale/update-all.sh
@@ -3,19 +3,21 @@
 echo "Generating new template..."
 `which python2` ./xml_po_parser.py ../data/gui/core_language_en_tag.xml ./stuntrally.pot
 
-echo "Fetching new translations..."
-(
-	# Clone the LP translation branch if it doesn't exist,
-	# update otherwise
-	if [ ! -d ./translations-export ]; then
-		mkdir ./translations-export
-		cd ./translations-export
-		bzr branch lp:~stuntrally-team/stuntrally/pofiles
-	else
-		cd ./translations-export/pofiles
-		bzr pull
-	fi
-)
+if [ "$1" != "--no-fetch" ]; then
+	echo "Fetching new translations..."
+	(
+		# Clone the LP translation branch if it doesn't exist,
+		# update otherwise
+		if [ ! -d ./translations-export ]; then
+			mkdir ./translations-export
+			cd ./translations-export
+			bzr branch lp:~stuntrally-team/stuntrally/pofiles
+		else
+			cd ./translations-export/pofiles
+			bzr pull
+		fi
+	)
+fi
 
 echo "Generating languages..."
 LOCALES="de fi ro pl fr ru"
