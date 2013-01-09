eca6bdd20efac31767f2bec74eb6ea48148f12a2
yours3lf <yours3lf@yours3lf-HP.(none)>
2013-01-09 23:27:19 +0100

    shadow fix

--- data/materials/shadows.h
+++ data/materials/shadows.h
@@ -5,15 +5,22 @@
 
 float depthShadowPCF (shTexture2D shadowMap, float4 shadowMapPos, float2 offset)
 {
-    shadowMapPos /= shadowMapPos.w;
-    //float3 o = float3(offset.xy, -offset.x) * 0.3;
+    float4 smp = shadowMapPos / shadowMapPos.w;
+    //shadowMapPos /= shadowMapPos.w;
+    
+		//float3 o = float3(offset.xy, -offset.x) * 0.3;
     float3 o = float3(0.0005, 0.0005, 0.0005);
     
     
-    float c =   (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy - o.xy).r) ? 1 : 0; // top left
+    float c =   (smp.z <= FIXED_BIAS + shSample(shadowMap, smp.xy - o.xy).r) ? 1 : 0; // top left
+    c +=        (smp.z <= FIXED_BIAS + shSample(shadowMap, smp.xy + o.xy).r) ? 1 : 0; // bottom right
+    c +=        (smp.z <= FIXED_BIAS + shSample(shadowMap, smp.xy + o.zy).r) ? 1 : 0; // bottom left
+    c +=        (smp.z <= FIXED_BIAS + shSample(shadowMap, smp.xy - o.zy).r) ? 1 : 0; // top right
+		
+		/*float c =   (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy - o.xy).r) ? 1 : 0; // top left
     c +=        (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy + o.xy).r) ? 1 : 0; // bottom right
     c +=        (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy + o.zy).r) ? 1 : 0; // bottom left
-    c +=        (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy - o.zy).r) ? 1 : 0; // top right
+    c +=        (shadowMapPos.z <= FIXED_BIAS + shSample(shadowMap, shadowMapPos.xy - o.zy).r) ? 1 : 0; // top right*/
     return c / 4;
     
 }
