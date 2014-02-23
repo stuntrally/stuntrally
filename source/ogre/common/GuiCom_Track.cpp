fa8de654ceb6ed552fe8611548d52bd40fef0332
CryHam <cryham@gmail.com>
2014-02-23 12:58:28 +0100

    upd scn clr

--- source/ogre/common/GuiCom_Track.cpp
+++ source/ogre/common/GuiCom_Track.cpp
@@ -46,12 +46,12 @@ String CGuiCom::GetSceneryColor(String name)
 	if (name.empty())  return "#707070";
 	if (name.c_str()[0]=='*')  name = name.substr(1);
 
-	String c = "#D0FFFF";  char ch = name.c_str()[0];
+	String c = "#B0E0E0";  char ch = name.c_str()[0];
 	switch (ch)  {
 		case 'T':  c = (name.c_str()[1] != 'e') ? "#FFA020" : 
 			(name.length() > 5 && name.c_str()[4] == 'C') ? "#A0C0D0" : "#A0A0A0";  break;  // Test,TestC
 		case 'J':  c = "#50FF50";  break;  case 'S':  c = "#C0E080";  break;  case 'F':  c = "#A0C000";  break;
-		case 'G':  c = "#B0FF00";  break;  case 'W':  c = "#C0D0D0";  break;  case 'I':  c = "#FFFF80";  break;
+		case 'G':  c = "#B0FF00";  break;  case 'W':  c = "#D0D8D8";  break;  case 'I':  c = "#FFFF80";  break;
 		case 'A':  c = "#FFA080";  break;  case 'D':  c = "#F0F000";  break;  case 'C':  c = "#E0B090";  break;
 		case 'V':  c = "#908030";  break;  case 'X':  c = "#8080D0";  break;  case 'M':  c = "#A0A000";  break;
 		case 'O':  c = "#70F0B0";  break;  case 'E':  c = "#A0E080";  break;  case 'R':  c = "#A04840";  break;  }
