48f8ce81727fbed522d331b0aef965484799d57e
CryHam <cryham@gmail.com>
2014-04-05 21:39:14 +0200

    tab close pick always

--- source/editor/Update_Key.cpp
+++ source/editor/Update_Key.cpp
@@ -108,11 +108,14 @@ bool App::keyPressed(const SDL_KeyboardEvent &arg)
 	///  Pick open  ---------------------
 	bool editGui = bGuiFocus && !pSet->isMain && pSet->inMenu==WND_Edit;
 	if (skey==key(TAB) && editGui)
-	switch (tab->getIndexSelected())
-	{	case TAB_Layers: gui->btnPickTex(0);  return true;
-		case TAB_Grass:  if (sub->getIndexSelected()==1)  gui->btnPickGrass(0);  return true;
-		case TAB_Veget:  if (sub->getIndexSelected()==1)  gui->btnPickVeget(0);  return true;
-		case TAB_Road:  if (sub->getIndexSelected()==0)  gui->btnPickRoad(0);  return true;
+	{
+		switch (tab->getIndexSelected())
+		{	case TAB_Layers:  gui->btnPickTex(0);  return true;
+			case TAB_Grass:  if (sub->getIndexSelected()==1) {  gui->btnPickGrass(0);  return true;  }  break;
+			case TAB_Veget:  if (sub->getIndexSelected()==1) {  gui->btnPickVeget(0);  return true;  }  break;
+			case TAB_Road:   if (sub->getIndexSelected()==0) {  gui->btnPickRoad(0);   return true;  }  break;
+		}
+		mWndPick->setVisible(false);
 	}
 
 	//  Global keys
