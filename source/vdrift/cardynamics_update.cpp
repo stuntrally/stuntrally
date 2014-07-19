859b372ea2eb854bd312212fa1ca9a1f556e4d78
CryHam <cryham@gmail.com>
2014-07-19 18:40:40 +0200

    fix O rear, stats

--- source/vdrift/cardynamics_update.cpp
+++ source/vdrift/cardynamics_update.cpp
@@ -811,16 +811,17 @@ void CARDYNAMICS::SimulateSphere(Dbl dt)
 	}
 
 	//  engine
-	//if (transmission.GetGear() < 0)  f *= -1.f;
+	bool rear = transmission.GetGear() < 0;
 	MATHVECTOR<Dbl,3> sv = -GetVelocity();
 	(-Orientation()).RotateVector(sv);
 	float vel = sv.Magnitude(),  //  decrease power with velocity
 		velMul = 1.f - std::min(1.f, hov.engineVelDec * vel);
 	float f = hov.engineForce * velMul * hov_throttle * dmgE
 			- hov.brakeForce * brake[0].GetBrakeFactor() * dmgE;
+	if (rear)  f *= -1.f;
 
 	//  steer  rotate dir
-	float pst = hov.steerForce;
+	float pst = hov.steerForce;  if (rear)  pst = -pst;
 		if (pipe)  pst *= hov.steerDampP;
 	sphereYaw += steerValue * dt * pst * PI_d/180.f;
 	MATHVECTOR<Dbl,3> dir(cosf(sphereYaw), -sinf(sphereYaw), 0);
