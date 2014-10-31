8cdad645f1ac5ac06c6acab4c48d085ebebc2561
CryHam <cryham@gmail.com>
2014-10-31 22:35:31 +0100

    upd, new champs, diff

--- dist/make_roadstats_xml.py
+++ dist/make_roadstats_xml.py
@@ -21,7 +21,7 @@ for line in times:
 	if len(line) > 0 and line[0] >= '0' and line[0] <= '9':
 		tr = r.split(line)
 		trk = tr[1]
-		print(tr)
+		#print(tr)
 		tim = tr[19]
 		map[trk] = tim
 		#print(trk + " " + tim)
