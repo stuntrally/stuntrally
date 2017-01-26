b0f97cb6ffd4e8cf8adaedbc32765244035beac3
CryHam <cryham@gmail.com>
2017-01-26 22:28:18 +0100

    use isnan define always, travis? pls check

--- source/vdrift/cardefs.h
+++ source/vdrift/cardefs.h
@@ -40,10 +40,10 @@ enum EPerfTest {PT_StartWait, PT_Accel, PT_Brake};
 
 const int Ncrashsounds = 12, Nwatersounds = 3;
 
-#ifdef _WIN32
+//#ifdef _WIN32
 static bool isnan(float number)  {  return (number != number);  }
 static bool isnan(double number) {  return (number != number);  }
-#endif
+//#endif
 
 const static char sCfgWh[MAX_WHEELS][4] = {"FL","FR","RL","RR","RL2","RR2","RL3","RR3"};  // .car config wheel names
 
