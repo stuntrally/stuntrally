3dd1e1a270c432546c86e1eb4571cb6ba82f46f9
CryHam <cryham@gmail.com>
2017-01-28 11:56:57 +0100

    Revert "use isnan define always, travis? pls check"
    This reverts commit b0f97cb6ffd4e8cf8adaedbc32765244035beac3.
    
    Conflicts:
    	source/vdrift/cardefs.h

--- source/vdrift/cardefs.h
+++ source/vdrift/cardefs.h
@@ -41,10 +41,8 @@ enum EPerfTest {PT_StartWait, PT_Accel, PT_Brake};
 const int Ncrashsounds = 12, Nwatersounds = 3;
 
 #ifdef _WIN32
-	static bool isnan(float number)  {  return (number != number);  }
-	static bool isnan(double number) {  return (number != number);  }
-#else
-	#define isnan(number)  std::isnan(number)
+static bool isnan(float number)  {  return (number != number);  }
+static bool isnan(double number) {  return (number != number);  }
 #endif
 
 const static char sCfgWh[MAX_WHEELS][4] = {"FL","FR","RL","RR","RL2","RR2","RL3","RR3"};  // .car config wheel names
