68f19ef3718f7fcbef00385496c55969ea2ff433
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-07-24 17:25:00 +0300

    Don't try to initialize haptic
    This make the game fail to start on FreeBSD, because SDL does not support haptic there. SDL_haptic doesn't seem to be used in the game anyway.

--- source/ogre/BaseApp_Create.cpp
+++ source/ogre/BaseApp_Create.cpp
@@ -254,7 +254,7 @@ bool BaseApp::configure()
 
 	mRoot->initialise(false);
 
-	Uint32 flags = SDL_INIT_VIDEO|SDL_INIT_JOYSTICK|SDL_INIT_HAPTIC|SDL_INIT_NOPARACHUTE;
+	Uint32 flags = SDL_INIT_VIDEO|SDL_INIT_JOYSTICK|SDL_INIT_NOPARACHUTE;
 	if (SDL_WasInit(flags) == 0)
 	{
 		SDL_SetHint(SDL_HINT_RENDER_DRIVER, "software");
