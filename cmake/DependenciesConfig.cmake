362ebf1140cabb5414cf775a1425b84a34863c15
Edgar <Edgar@AnotherFoxGuy.com>
2021-10-22 10:55:20 +0200

    :bug: Fixed some small issues

--- cmake/DependenciesConfig.cmake
+++ cmake/DependenciesConfig.cmake
@@ -9,29 +9,6 @@ add_external_lib(
         boost/1.76.0
         REQUIRED
         FIND_PACKAGE_OPTIONS COMPONENTS system thread filesystem wave
-        # Remove unused boost stuff
-        CONAN_OPTIONS
-        boost:without_context=True
-        boost:without_contract=True
-        boost:without_coroutine=True
-        boost:without_fiber=True
-        boost:without_graph=True
-        boost:without_graph_parallel=True
-        boost:without_iostreams=True
-        boost:without_json=True
-        boost:without_locale=True
-        boost:without_log=True
-        boost:without_math=True
-        boost:without_mpi=True
-        boost:without_nowide=True
-        boost:without_program_options=True
-        boost:without_python=True
-        boost:without_random=True
-        boost:without_regex=True
-        boost:without_stacktrace=True
-        boost:without_test=True
-        boost:without_timer=True
-        boost:without_type_erasure=True
 )
 
 add_external_lib(
--- cmake/dependencies-helpers/Boost.cmake
+++ cmake/dependencies-helpers/Boost.cmake
@@ -10,4 +10,15 @@ if (${_PREFIX}USE_BOOST STREQUAL "SYSTEM")
                 INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIRS}"
                 )
     endif ()
+
+elseif(${_PREFIX}USE_BOOST STREQUAL "CONAN")
+    get_target_property(Boost_LIBRARIES CONAN_PKG::boost INTERFACE_LINK_LIBRARIES)
+    foreach(conf IN ITEMS release debug)
+        list(REMOVE_ITEM Boost_LIBRARIES 
+            "CONAN_LIB::boost_boost_unit_test_framework${conf}" 
+            "CONAN_LIB::boost_boost_test_exec_monitor${conf}"
+            "CONAN_LIB::boost_boost_prg_exec_monitor${conf}"
+        )
+    endforeach()
+    set_target_properties(CONAN_PKG::boost PROPERTIES INTERFACE_LINK_LIBRARIES "${Boost_LIBRARIES}")
 endif ()
