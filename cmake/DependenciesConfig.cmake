903cb3c27e87e0bdb6d6103edd3d14762f1f9bf3
Edgar <Edgar@AnotherFoxGuy.com>
2023-04-10 12:45:48 +0200

    :wrench: Fixed build with Conan V2

--- cmake/DependenciesConfig.cmake
+++ cmake/DependenciesConfig.cmake
@@ -1,124 +1,33 @@
-include(DependenciesFunctions)
 set(CMAKE_THREAD_PREFER_PTHREAD YES)
 find_package(Threads REQUIRED)
 
-if (USE_PACKAGE_MANAGER)
-    conan_add_remote(NAME ror-conan
-            URL https://git.anotherfoxguy.com/api/packages/rorbot/conan
-            VERIFY_SSL True
-            )
-endif ()
 
-# Some pkg-config files are broken, that is why they are commented out
+find_package(Boost REQUIRED)
+find_package(Bullet REQUIRED)
+find_package(OGRE REQUIRED)
+find_package(SDL2 REQUIRED)
+find_package(MyGUI REQUIRED)
+find_package(OGG REQUIRED)
+find_package(Vorbis REQUIRED)
+find_package(Tinyxml REQUIRED)
+find_package(Tinyxml2 REQUIRED)
 
-add_external_lib(
-        Boost
-        boost/1.80.0
-        REQUIRED
-        FIND_PACKAGE_OPTIONS COMPONENTS system thread filesystem wave
-)
+find_package(OpenAL REQUIRED)
+find_package(ENet REQUIRED)
 
-add_external_lib(
-        OGRE
-        ogre3d/13.5.3@anotherfoxguy/stable
-        REQUIRED
-        CONAN_OPTIONS ogre3d:nodeless_positioning=True ogre3d:resourcemanager_strict=off
-        #  uncomment below for Ogre 1.9
-        #PKG_CONFIG "OGRE, OGRE-Terrain, OGRE-Paging, OGRE-Overlay"        
-        FIND_PACKAGE_OPTIONS CONFIG COMPONENTS Bites Overlay Paging RTShaderSystem MeshLodGenerator Terrain
-)
-
-add_external_lib(
-        BULLET
-        bullet3/3.24
-        REQUIRED
-        FIND_PACKAGE
-        CONAN_OPTIONS bullet3:extras=True bullet3:network_support=True
-        INTERFACE_NAME Bullet::Bullet
-)
-
-add_external_lib(
-        SDL2
-        sdl/2.24.1
-        REQUIRED
-        PKG_CONFIG "sdl2 >= 2.0"
-        FIND_PACKAGE_OPTIONS CONFIG
-        CONAN_OPTIONS sdl:sdl2main=False
-)
-
-add_external_lib(
-        MyGUI
-        mygui/3.4.1@anotherfoxguy/stable
-        REQUIRED
-        # PKG_CONFIG "MYGUI = 3.4.0"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        OGG
-        ogg/1.3.5
-        REQUIRED
-        PKG_CONFIG "ogg >= 1.2"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        VorbisFile
-        vorbis/1.3.7
-        REQUIRED
-        PKG_CONFIG "vorbis >= 1.2, vorbisfile >= 1.2"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        OpenAL
-        openal/1.22.2
-        PKG_CONFIG "openal >= 1.18"
-        FIND_PACKAGE_OPTIONS CONFIG
-)
-
-add_external_lib(
-        ENet
-        enet/1.3.17
-        REQUIRED
-        PKG_CONFIG "libenet >= 1.2"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        tinyxml
-        tinyxml/2.6.2
-        REQUIRED
-        PKG_CONFIG "tinyxml >= 2.6"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        tinyxml2
-        tinyxml2/9.0.0
-        REQUIRED
-        PKG_CONFIG "tinyxml2 >= 6"
-        FIND_PACKAGE
-)
-
-# Fix conan version conflict
-add_external_lib(
-        libpng
-        libpng/1.6.38
-)
-
-set(LIBS Boost::Boost
+set(LIBS boost::boost
         Threads::Threads
         OGRE::OGRE
         Bullet::Bullet
         SDL2::SDL2
         MyGUI::MyGUI
-        VorbisFile::VorbisFile
-        OGG::OGG
+        Vorbis::vorbis
+        Vorbis::vorbisfile
+        Ogg::ogg
         OpenAL::OpenAL
-        ENet::ENet
+        enet::enet
         tinyxml::tinyxml
         tinyxml2::tinyxml2
 )
 
-set(SERVER_LIBS Boost::Boost ENet::ENet)
+set(SERVER_LIBS boost::boost enet::enet)
---
+++ conanfile.py
@@ -0,0 +1,46 @@
+import os
+from conan import ConanFile
+from conan.tools.files import copy
+
+
+class StuntRally(ConanFile):
+    name = "StuntRally"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+    default_options = {
+        "ogre3d*:nodeless_positioning": "True",
+        "ogre3d*:resourcemanager_strict": "off",
+        "bullet3*:extras": "True",
+        "bullet3*:network_support": "True",
+        "sdl*:sdl2main": "False",
+        "ogre3d*:nodeless_positioning": "True",
+    }
+
+    def requirements(self):
+        self.requires("boost/1.81.0")
+        self.requires("ogre3d/13.6.4@anotherfoxguy/stable")
+        self.requires("bullet3/3.25")
+        self.requires("sdl/2.26.1")
+        self.requires("mygui/3.4.1@anotherfoxguy/stable")
+        self.requires("ogg/1.3.5")
+        self.requires("vorbis/1.3.7")
+        self.requires("openal/1.22.2")
+        self.requires("enet/1.3.17")
+        self.requires("tinyxml/2.6.2")
+        self.requires("tinyxml2/9.0.0")
+
+        self.requires("libpng/1.6.39", override=True)
+        self.requires("libwebp/1.3.0", override=True)
+        self.requires("zlib/1.2.13", override=True)
+
+    def generate(self):
+        for dep in self.dependencies.values():
+            for f in dep.cpp_info.bindirs:
+                self.cp_data(f)
+            for f in dep.cpp_info.libdirs:
+                self.cp_data(f)
+
+    def cp_data(self, src):
+        bindir = os.path.join(self.build_folder, "bin")
+        copy(self, "*.dll", src, bindir, False)
+        copy(self, "*.so*", src, bindir, False)
