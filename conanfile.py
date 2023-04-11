02ecd616ad3d1d219853f889c30180f40d47fe0c
Edgar <Edgar@AnotherFoxGuy.com>
2023-04-11 10:58:23 +0200

    :wrench: Use patched bullet3 conan pkg

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class StuntRally(ConanFile):
     def requirements(self):
         self.requires("boost/1.81.0")
         self.requires("ogre3d/13.6.4@anotherfoxguy/stable")
-        self.requires("bullet3/3.25")
+        self.requires("bullet3/3.25@anotherfoxguy/patched")# Needs a patched to build on windows 
         self.requires("sdl/2.26.1")
         self.requires("mygui/3.4.1@anotherfoxguy/stable")
         self.requires("ogg/1.3.5")
